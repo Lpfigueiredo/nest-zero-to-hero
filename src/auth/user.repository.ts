effffb430fd8ff2cf81bfb82ddea215b9b5608ae
Leonardo Figueiredo <leommko@gmail.com>
2021-02-24 00:59:36 -0300

    feat: add auth routes

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -43,6 +43,6 @@ export class UserRepository extends Repository<User> {
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
-    return bcrypt.hash(password, salt);
+    return await bcrypt.hash(password, salt);
   }
 }
